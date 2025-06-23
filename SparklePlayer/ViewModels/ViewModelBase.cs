7f334366796d5f3c921498e9f3f3166728d47263
Tianyi,HE <163421589+BegoniaHe@users.noreply.github.com>
2025-06-24 01:08:18 +0200

    Initial commit with Avalonia SparklePlayer project
    Add initial project structure for SparklePlayer using Avalonia UI, including solution files, main application, view models, views, resources, and configuration files. Includes .gitignore, project manifest, and Qodana configuration.

---
+++ SparklePlayer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SparklePlayer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ SparklePlayer/readme.md
@@ -0,0 +1,3 @@
+# Sparkle Player
+
+本项目是Sparkle Player的C#版本, 原版是用java awt编写的, 主要用于播放音频文件.
